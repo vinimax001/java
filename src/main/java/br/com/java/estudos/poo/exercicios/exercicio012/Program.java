382384822eeef557a75bddf383b7229c0d8f4aa9
vinimax <vini.ciusang@gmail.com>
2025-11-10 10:37:48 -0300

    feat:exercises 012

---
+++ src/main/java/br/com/java/estudos/poo/exercicios/exercicio012/Program.java
@@ -0,0 +1,41 @@
+package br.com.java.estudos.poo.exercicios.exercicio012;
+
+import br.com.java.estudos.poo.exercicios.exercicio012.entities.Rent;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        Rent[] rents = new Rent[10];
+
+        System.out.print("How many rooms will be rented:? ");
+        int n  = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            System.out.println("Rent #" + (i + 1) + ":");
+            sc.nextLine();
+            System.out.print("Name: ");
+            String name = sc.nextLine();
+            System.out.print("Email: ");
+            String email = sc.nextLine();
+            System.out.print("Room: ");
+            int num = sc.nextInt();
+            rents[num] = new Rent(name, email);
+        }
+
+        System.out.println();
+        System.out.println("Busy rooms:");
+        for (int i = 0; i < rents.length; i++) {
+            if (rents[i] != null) {
+                System.out.println( i + ": " + rents[i].getName() + ", " + rents[i].getEmail());
+            }
+        }
+
+        sc.close();
+    }
+}
---
+++ src/main/java/br/com/java/estudos/poo/exercicios/exercicio012/entities/Rent.java
@@ -0,0 +1,29 @@
+package br.com.java.estudos.poo.exercicios.exercicio012.entities;
+
+public class Rent {
+
+    private String name;
+    private String email;
+
+    public Rent(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public  String getName() {
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail () {
+        this.email = email;
+    }
+
+}
