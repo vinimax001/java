e20759746113711d4f35d2878a5d11bd6c4b3b1e
vinimax <vini.ciusang@gmail.com>
2025-11-10 08:39:43 -0300

    feat:added lesson012

---
+++ src/main/java/br/com/java/estudos/poo/aulas/aula012/Product/Product.java
@@ -0,0 +1,7 @@
+package br.com.java.estudos.poo.aulas.aula012.Product;
+
+public class Product {
+    private String name;
+    private Double price;
+
+}
---
+++ src/main/java/br/com/java/estudos/poo/aulas/aula012/Program.java
@@ -0,0 +1,14 @@
+package br.com.java.estudos.poo.aulas.aula012;
+
+public class Program {
+    public static void main(String[] args) {
+        int x = 20;
+
+        Integer obj = x;
+        int y =  obj;
+
+        System.out.println(x);
+        System.out.println(y * 2);
+
+    }
+}
